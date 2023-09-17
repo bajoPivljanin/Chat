de08050a414e7a016ffe04bc6200f85a9d9896b0
bajoPivljanin <103775541+bajoPivljanin@users.noreply.github.com>
2023-09-17 21:20:12 +0200

    firebase commit

--- firebase.js
+++ firebase.js
@@ -1,5 +1,35 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from 'firebase'
+// import * as firebase from 'firebase'
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCvsYbzEEDnAySIZ4mgzFT1gqZuqhACrhc",
+//   authDomain: "chat-6c3ca.firebaseapp.com",
+//   projectId: "chat-6c3ca",
+//   storageBucket: "chat-6c3ca.appspot.com",
+//   messagingSenderId: "938740113378",
+//   appId: "1:938740113378:web:d73d1a670ae71acfb8e3bb"
+// };
+
+// // Initialize Firebase
+// let app;
+// if (firebase.apps.length === 0) {
+//   app = firebase.initializeApp(firebaseConfig);
+// } else {
+//   app = firebase.app()
+// }
+
+// const auth = firebase.auth()
+
+// export { auth };
+
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,12 +45,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 let app;
+
 if (firebase.apps.length === 0) {
   app = firebase.initializeApp(firebaseConfig);
 } else {
-  app = firebase.app()
+  app = firebase.app();
 }
 
-const auth = firebase.auth()
+const db = app.firestore();
+const auth = firebase.auth();
 
-export { auth };
+export { db, auth };
